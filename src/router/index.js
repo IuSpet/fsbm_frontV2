912bef0c5337fc9c0afe7ba6ebff07de93427cde
luSpet <1037821259@qq.com>
2021-05-14 17:27:15 +0800

    add icons

--- src/router/index.js
+++ src/router/index.js
@@ -69,7 +69,7 @@ export const constantRoutes = [
         path: 'live_wall',
         name: 'LiveWall',
         component: () => import('@/views/video/LiveWall'),
-        meta: { title: '监控墙', icon: '' }
+        meta: { title: '监控墙', icon: 'monitor' }
       }
     ]
   },
@@ -79,25 +79,25 @@ export const constantRoutes = [
     component: Layout,
     redirect: '/admin/stats',
     name: 'Admin',
-    meta: { title: '管理员', icon: '', roles: ['admin'] },
+    meta: { title: '管理员', icon: 'admin', roles: ['admin'] },
     children: [
       {
         path: 'stats',
         name: 'Stats',
         component: () => import('@/views/admin/registerStats/index'),
-        meta: { title: '注册用户统计', icon: '', roles: ['admin'] }
+        meta: { title: '注册用户统计', icon: 'register-stats', roles: ['admin'] }
       },
       {
         path: 'user_list',
         name: 'UserList',
         component: () => import('@/views/admin/userTable/index'),
-        meta: { title: '用户列表', icon: '', roles: ['admin'] }
+        meta: { title: '用户列表', icon: 'table', roles: ['admin'] }
       },
       {
         path: 'user_detail',
         name: 'UserDetail',
         component: () => import('@/views/user/userDetail'),
-        meta: { title: '用户详细信息', icon: '', roles: ['admin'] },
+        meta: { title: '用户详细信息', icon: 'profile', roles: ['admin'] },
         hidden: true
       }
     ]
@@ -108,31 +108,31 @@ export const constantRoutes = [
     component: Layout,
     redirect: '/shop/shop_list',
     name: 'Shop',
-    meta: { title: '店铺与监控' },
+    meta: { title: '店铺与监控', icon: 'shop' },
     children: [
       {
         path: 'shop_list',
         name: 'ShopList',
         component: () => import('@/views/shop/shopList/index'),
-        meta: { title: '店铺列表' }
+        meta: { title: '店铺列表', icon: 'table' }
       },
       {
         path: 'add_shop',
         name: 'AddShop',
         component: () => import('@/views/shop/addShop'),
-        meta: { title: '注册店铺', roles: ['admin', 'manager'] }
+        meta: { title: '注册店铺', icon: 'register', roles: ['admin', 'manager'] }
       },
       {
         path: 'monitor_list',
         name: 'MonitorList',
         component: () => import('@/views/shop/monitorList'),
-        meta: { title: '监控列表' }
+        meta: { title: '监控列表', icon: 'table' }
       },
       {
         path: 'add_monitor',
         name: 'AddMonitor',
         component: () => import('@/views/shop/addMonitor'),
-        meta: { title: '注册监控', roles: ['admin', 'manager'] }
+        meta: { title: '注册监控', icon: 'register', roles: ['admin', 'manager'] }
       },
       {
         path: 'shop_detail',
@@ -147,20 +147,20 @@ export const constantRoutes = [
     component: Layout,
     redirect: '/alarm/alarm_list',
     name: 'Alarm',
-    meta: { title: '报警', icon: '' },
+    meta: { title: '报警', icon: 'alarm' },
     alwaysShow: true,
     children: [
       {
         path: '/alarm/alarm_list',
         name: 'AlarmList',
         component: () => import('@/views/alarm/AlarmList'),
-        meta: { title: '报警列表', icon: '' }
+        meta: { title: '报警列表', icon: 'table' }
       },
       {
         path: '/alarm/alarm_detail',
         name: 'AlarmDetail',
         component: () => import('@/views/alarm/AlarmDetail'),
-        meta: { title: '报警详情', icon: '' },
+        meta: { title: '报警详情', icon: 'profile' },
         hidden: true
       }
     ]
@@ -171,119 +171,22 @@ export const constantRoutes = [
     component: Layout,
     redirect: '/auth/apply_role',
     name: 'Auth',
-    meta: { title: '权限管理', icon: '' },
+    meta: { title: '权限管理', icon: 'auth' },
     children: [
       {
         path: '/apply_role',
         name: 'ApplyRole',
         component: () => import('@/views/auth/applyRole'),
-        meta: { title: '申请角色', icon: '' }
+        meta: { title: '申请角色', icon: 'apply' }
       },
       {
         path: '/apply_list',
         name: 'ApplyList',
         component: () => import('@/views/auth/applyList'),
-        meta: { title: '工单列表', icon: '', roles: ['admin'] }
+        meta: { title: '工单列表', icon: 'table', roles: ['admin'] }
       }
     ]
   },
-
-  // {
-  //   path: '/example',
-  //   component: Layout,
-  //   redirect: '/example/table',
-  //   name: 'Example',
-  //   meta: { title: 'Example', icon: 'el-icon-s-help' },
-  //   children: [
-  //     {
-  //       path: 'table',
-  //       name: 'Table',
-  //       component: () => import('@/views/table/index'),
-  //       meta: { title: 'Table', icon: 'table' }
-  //     },
-  //     {
-  //       path: 'tree',
-  //       name: 'Tree',
-  //       component: () => import('@/views/tree/index'),
-  //       meta: { title: 'Tree', icon: 'tree' }
-  //     }
-  //   ],
-  //   hidden: true
-  // },
-
-  // {
-  //   path: '/form',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'index',
-  //       name: 'Form',
-  //       component: () => import('@/views/form/index'),
-  //       meta: { title: 'Form', icon: 'form' }
-  //     }
-  //   ],
-  //   hidden: true
-  // },
-
-  // {
-  //   path: '/nested',
-  //   component: Layout,
-  //   redirect: '/nested/menu1',
-  //   name: 'Nested',
-  //   meta: {
-  //     title: 'Nested',
-  //     icon: 'nested'
-  //   },
-  //   children: [
-  //     {
-  //       path: 'menu1',
-  //       component: () => import('@/views/nested/menu1/index'), // Parent router-view
-  //       name: 'Menu1',
-  //       meta: { title: 'Menu1' },
-  //       children: [
-  //         {
-  //           path: 'menu1-1',
-  //           component: () => import('@/views/nested/menu1/menu1-1'),
-  //           name: 'Menu1-1',
-  //           meta: { title: 'Menu1-1' }
-  //         },
-  //         {
-  //           path: 'menu1-2',
-  //           component: () => import('@/views/nested/menu1/menu1-2'),
-  //           name: 'Menu1-2',
-  //           meta: { title: 'Menu1-2' },
-  //           children: [
-  //             {
-  //               path: 'menu1-2-1',
-  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-  //               name: 'Menu1-2-1',
-  //               meta: { title: 'Menu1-2-1' }
-  //             },
-  //             {
-  //               path: 'menu1-2-2',
-  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-  //               name: 'Menu1-2-2',
-  //               meta: { title: 'Menu1-2-2' }
-  //             }
-  //           ]
-  //         },
-  //         {
-  //           path: 'menu1-3',
-  //           component: () => import('@/views/nested/menu1/menu1-3'),
-  //           name: 'Menu1-3',
-  //           meta: { title: 'Menu1-3' }
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       path: 'menu2',
-  //       component: () => import('@/views/nested/menu2/index'),
-  //       name: 'Menu2',
-  //       meta: { title: 'menu2' }
-  //     }
-  //   ],
-  //   hidden: true
-  // },
 
   {
     path: '/profile',
@@ -293,22 +196,10 @@ export const constantRoutes = [
         path: 'index',
         component: () => import('@/views/profile/index'),
         name: 'Profile',
-        meta: { title: '用户资料', icon: '' }
+        meta: { title: '用户资料', icon: 'profile' }
       }
     ]
   },
-
-  // {
-  //   path: 'external-link',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-  //       meta: { title: 'External Link', icon: 'link' }
-  //     }
-  //   ],
-  //   hidden: true
-  // },
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
